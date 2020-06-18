c11baebfbdb39bb87ee6023c27affddddb73c7f1
Hezonar <59221206+Hezonar@users.noreply.github.com>
2020-06-18 22:47:17 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,24 @@
+import config
+import telebot
+import random
+from telebot import types
+#import Start
+import time
+
+bot = telebot.TeleBot(config.API_TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start_script(message):
+    bot.send_message(message.chat.id, "Скрипт успешно запущен")
+    time.sleep(1)
+    #Start.main(1)
+
+
+@bot.message_handler(commands=['about'])
+def send_about(message):
+    bot.send_message(message.chat.id, "Долбоеб напиши /start")
+
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
