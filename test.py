697acfd64922ee8f0e06777b6aa2649cacf76410
Hezonar <59221206+Hezonar@users.noreply.github.com>
2020-06-18 23:12:14 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
+from xvfbwrapper import Xvfb
+vdisplay = Xvfb()
+vdisplay.start()
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-setuid-sandbox")
+driver = webdriver.Chrome('chromedriver.exe', chrome_options=chrome_options)
+driver.get('https://google.com')
+ff = open("file.txt", "w")
+ff.write(driver.page_source.encode("utf-8"))
+ff.close()
+driver.close()
+vdisplay.stop()
